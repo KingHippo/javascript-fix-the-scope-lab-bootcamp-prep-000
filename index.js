a2cfc9961b6676d4bdee69d07b9abfa8f8fcf6c6
askdonno@gmail.com <askdonno@gmail.com>
2018-07-12 05:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var animal = 'dog'
 
 function myAnimal() {
+  var animal = 'dog';
   return animal;
 }
 
@@ -28,5 +28,5 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction();
 
-yourAnimal('cat');
+
 theFunk();
